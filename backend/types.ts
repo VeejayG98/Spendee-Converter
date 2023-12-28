eabf652f139ec5be6b835c24873ea2aeb5b43fd9
jayasuryaa-govindraj <jayasuryaa.govindraj1998@gmail.com>
2023-12-27 18:14:39 -0600

    feat: Add Transaction and CardData types to backend/types.ts and create processDiscover function in backend/utils/discover.ts

--- backend/types.ts
+++ backend/types.ts
@@ -1,6 +1,12 @@
+export type Transaction = {
+  date: string;
+  description: string;
+  amount: number;
+  category: string;
+};
+
 export type CardData = {
   discover: string;
   chase: string;
   apple: string;
-}
-
+};
---
+++ backend/utils/discover.ts
@@ -0,0 +1,30 @@
+import { Transaction } from "../types";
+
+export default function processDiscover(data: string | null): Transaction[] {
+  const jsonData: Transaction[] = [];
+
+  if (!data) return jsonData;
+
+  const rows = data.split("\r\n");
+
+  const mapping = {
+    date: 0,
+    description: 2,
+    amount: 3,
+    category: 4,
+  };
+
+  for (let i = 1; i < rows.length - 1; i++) {
+    const cells = rows[i].split(",");
+
+    const row: Transaction = {
+      date: cells[mapping.date],
+      description: cells[mapping.description].replace(/"/g, ""),
+      amount: parseFloat(cells[mapping.amount]),
+      category: cells[mapping.category].replace(/"/g, ""),
+    };
+
+    if (row.amount >= 0) jsonData.push(row);
+  }
+  return jsonData;
+}
