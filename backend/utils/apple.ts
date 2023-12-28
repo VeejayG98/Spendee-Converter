464f444c0a9e012d3d8a0ab1c43f7dfdd213ab46
jayasuryaa-govindraj <jayasuryaa.govindraj1998@gmail.com>
2023-12-27 18:17:32 -0600

    feat: Add processApple function to handle Apple transactions

---
+++ backend/utils/apple.ts
@@ -0,0 +1,31 @@
+import { Transaction } from "../types";
+
+export default function processApple(data: string | null): Transaction[] {
+  const jsonData: Transaction[] = [];
+
+  if (!data) return jsonData;
+
+  const rows = data.split("\n");
+
+  const mapping = {
+    date: 0,
+    description: 2,
+    amount: 6,
+    category: 4,
+  };
+
+  for (let i = 1; i < rows.length - 1; i++) {
+    const cells = rows[i].split(",");
+
+    const row: Transaction = {
+      date: cells[mapping.date],
+      description: cells[mapping.description].replace(/"/g, ""),
+      amount: parseFloat(cells[mapping.amount].replace(/"/g, "")),
+      category: cells[mapping.category].replace(/"/g, ""),
+    };
+
+    if (row.amount >= 0) jsonData.push(row);
+  }
+
+  return jsonData;
+}
